af7b066e59e4d8ca0e20c29a8045414ca01c210f
axeliono <69375446+axeliono@users.noreply.github.com>
2020-10-25 17:44:30 -0500

    finally completed time/color functionality

--- Develop/scheduler.js
+++ Develop/scheduler.js
@@ -1,19 +1,40 @@
 var currentTime = moment().format("LT");
 console.log(currentTime);
+var rowAmt = document.getElementsByClassName("hour");
 var rowArray = [];
-rowArray = $(".container").children("hour");
+
+//put all row elements into array
+rowArray.push($(".row"));
 
 //show current date in Jumbotron
 $("#currentDay").text(moment().format("LL"));
 
 
-rowArray.forEach(checkTime);
+//function that goes to every row and compares it's time id with the current time
+var checkTime = function(rowItem) {
+    rowTime = rowItem.children(".hour").attr("id");
 
-
-var checkTime = function() {
-    let time = moment(rowItem.text())
+    let time = moment(rowTime, "HH:mm:ss");
     console.log(time);
     if(time.isBefore(moment())) {
-        rowItem.addClass("past");
+        if((time.diff(moment()), 'hours') < 1){
+            rowItem.children(".description").addClass("present");
+        }
+        else {
+            rowItem.children(".description").addClass("past");
+        }
+    }
+    else {
+        rowItem.children(".description").addClass("future");
     }
 };
+
+//function to check each row in array
+var checkRowArray = function() {
+
+    for(i = 0; i < rowArray.length; i++) {
+        checkTime(rowArray[i]);
+    }
+}
+
+checkRowArray();
