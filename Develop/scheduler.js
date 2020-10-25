9b2d77c1a21fdd96c39639d1cc5610af05c67bdc
Chandler Green <chandleregreen@yahoo.com>
2020-10-25 14:14:29 -0500

    working on adding color based on relation to current time

--- Develop/scheduler.js
+++ Develop/scheduler.js
@@ -1,4 +1,19 @@
-var currentTime = moment();
+var currentTime = moment().format("LT");
+console.log(currentTime);
+var rowArray = [];
+rowArray = $(".container").children("hour");
 
 //show current date in Jumbotron
-$("#currentDay").text() = moment().format("LL");
+$("#currentDay").text(moment().format("LL"));
+
+
+rowArray.forEach(checkTime);
+
+
+var checkTime = function() {
+    let time = moment(rowItem.text())
+    console.log(time);
+    if(time.isBefore(moment())) {
+        rowItem.addClass("past");
+    }
+};
